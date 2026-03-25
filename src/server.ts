5e3ecf87e01879925cd3ffc024b0cd91ecb9f64e
rohit-p-droid <76893151+rohit-p-droid@users.noreply.github.com>
2026-03-26 00:02:18 +0530

    project setup

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { config } from "./config";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+const PORT = config.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
